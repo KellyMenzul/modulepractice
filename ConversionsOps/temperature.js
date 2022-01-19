8dcc3c25933fb78caf1e7faaa914b0aa6c7fba79
Hannah MacPherson <hannahmacpherson@Hannahs-MacBook-Pro.local>
2022-01-18 16:12:40 -0800

    added C to F converstion to temperature function

--- ConversionsOps/temperature.js
+++ ConversionsOps/temperature.js
@@ -1,5 +1,13 @@
-const temperature = (a)=>{
-    return ((a - 30) / 2)
+const temperature = (a, unit)=>{
+    //Farenhiet to Celcius
+    if(unit === "f" || unit === "F"){
+        return ((a - 30) / 2)
+    }
+
+    //Celcius to Farenhiet
+    if(unit === "c" || unit === "C"){
+        return ((a * 1.8000) +  32)
+    }
 }
 
 module.exports = temperature
